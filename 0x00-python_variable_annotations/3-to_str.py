4477c7ecbf84f52532046cc203beee3c70d06fb1
Ernest Push Gbaboh <gbaboh.ernest@gmail.com>
2024-03-14 23:54:05 +0000

    A function that takes a float value as argument and returns the string respresentation of the float value

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""Basic annotations - to string"""
+
+
+def to_str(n: float) -> str:
+    """
+    A function that takes a float
+    value and return the string
+    representation of the float value.
+    :param n: float value
+    :return: A string representation of n.
+    """
+    return str(n)

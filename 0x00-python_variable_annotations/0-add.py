c8d761124f4b82c67a003e9ca2c16ea0a08c92bb
Ernest Push Gbaboh <gbaboh.ernest@gmail.com>
2024-03-14 23:15:32 +0000

    Basic annotation - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""Basic annotation - add"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    A type-annotated function add that two
+    float value as argument and return their
+    sum as float.
+    :param a: float value.
+    :param b: float value.
+    :return: a + b
+    """
+    return a + b

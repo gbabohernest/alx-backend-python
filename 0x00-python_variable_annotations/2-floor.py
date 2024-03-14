14dcb48f300337710ddb715bc5f484cd58aa4d06
Ernest Push Gbaboh <gbaboh.ernest@gmail.com>
2024-03-14 23:44:48 +0000

    A function which takes a float argument and return the floor value of the float argument.

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""Basic annotations - floor"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    A function which tasks a float value as
+    arg and return the floor of the value
+    :param n: float value
+    :return: The floor value of the float
+    """
+    return math.floor(n)

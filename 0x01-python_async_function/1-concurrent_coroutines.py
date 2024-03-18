aa26a987973273da421c6e753a4785365fcfbcc8
Ernest Push Gbaboh <gbaboh.ernest@gmail.com>
2024-03-18 20:17:55 +0000

    Generate a list of random delays

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+"""Defines asynchronous coroutines for generating random delays."""
+
+from typing import List
+
+try:
+    wait_random = __import__('0-basic_async_syntax').wait_random
+except ImportError:
+    pass
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Generate a list of random delays.
+
+    :param n:(int) The number of random delays to generate.
+    :param max_delay:(int) The maximum delay in seconds.
+    :return: List[float]: A list of random delays as floats.
+    """
+    all_delays = []
+    for _ in range(n):
+        delay = await wait_random(max_delay)
+        all_delays.append(delay)
+    return all_delays

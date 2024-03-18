a407975f680e652101df2a5fe97da43e556a6e27
Ernest Push Gbaboh <gbaboh.ernest@gmail.com>
2024-03-18 21:14:39 +0000

    creates and return an asyncio.Task

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+"""
+    Defines a regular function that takes an integer
+    and return an asyncio.Task
+"""
+
+from typing import Any
+import asyncio
+
+try:
+    wait_random = __import__('0-basic_async_syntax').wait_random
+except ImportError:
+    pass
+
+
+def task_wait_random(max_delay: int) -> Any:
+    """
+     Creates and returns an asyncio.Task for the
+     wait_random coroutine with the given max_delay.
+
+    :param max_delay: (int) maximum delay in seconds for
+                      the wait coroutine.
+    :return: An asyncio.Task
+    """
+    return asyncio.create_task(wait_random(max_delay))

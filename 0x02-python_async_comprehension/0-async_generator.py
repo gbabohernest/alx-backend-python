ab72366a9023dbb2ffb11a1a18055fd0a9ba7342
Ernest Push Gbaboh <gbaboh.ernest@gmail.com>
2024-03-19 21:20:22 +0000

    change the generator ReturnType parameter

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,7 +7,7 @@ import asyncio
 import random
 
 
-async def async_generator() -> Generator[int, None, float]:
+async def async_generator() -> Generator[int, None, None]:
     """
     Defines an asynchronous generator that generates
     / yields random number between 0 and 10
